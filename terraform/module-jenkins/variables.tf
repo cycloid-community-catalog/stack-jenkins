cc51ac7d902cf1e421332934de1009234ac76961
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2019-12-11 16:03:37 +0100

    terraform: variables add default variables
    we do not need to use region for now

---
+++ terraform/module-jenkins/variables.tf
@@ -0,0 +1,64 @@
+variable "bastion_sg_allow" {
+}
+
+variable "env" {
+}
+
+variable "customer" {
+}
+
+variable "zones" {
+  type    = list(string)
+  default = []
+}
+
+variable "keypair_name" {
+  default = "cycloid"
+}
+
+variable "private_subnets_ids" {
+  type    = list(string)
+  default = [""]
+}
+
+variable "public_subnets_ids" {
+  type = list(string)
+}
+
+variable "vpc_id" {
+  default = ""
+}
+
+variable "project" {
+  default = "jenkins"
+}
+
+variable "extra_tags" {
+  default = {}
+}
+
+variable "jenkins_disk_size" {
+  default = 60
+}
+
+variable "jenkins_disk_type" {
+  default = "gp2"
+}
+
+variable "jenkins_type" {
+  default = "t3.small"
+}
+
+variable "jenkins_ebs_optimized" {
+  default = true
+}
+
+locals {
+  standard_tags = {
+    "cycloid.io" = "true"
+    env          = var.env
+    project      = var.project
+    customer     = var.customer
+  }
+  merged_tags = merge(local.standard_tags, var.extra_tags)
+}
